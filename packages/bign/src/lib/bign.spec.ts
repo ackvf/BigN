5251342312426af2b6b5140ba914ae1b3296510f
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qry.me>
2022-01-18 00:50:03 +0100

    Add BigInt helper class

--- packages/bign/src/lib/bign.spec.ts
+++ packages/bign/src/lib/bign.spec.ts
@@ -1,7 +1,186 @@
-import { bign } from './bign';
+import { N } from './bign'
 
-describe('bign', () => {
-  it('should work', () => {
-    expect(bign()).toEqual('bign');
-  });
-});
+// Make BigInt serializable with `JSON.stringify()`
+(BigInt.prototype as any).toJSON = function () { return this.toString() }
+
+expect.extend({
+  toExactlyMatchN(a: N, b: N) {
+    let pass = true
+
+    /* TODO: Maybe refine this later. What exactly should match for two bigints to be equal? Only their Integer part or also decimal precision? */
+    if (a.value !== b.value) pass = false
+    if (a.precision !== b.precision) pass = false
+    if (a.factor !== b.factor) pass = false
+
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${a.toString()} not to be same as ${b.toString()}`,
+        pass: true,
+      }
+    } else {
+      return {
+        message: () =>
+          `expected ${b.toString()} to be same as ${b.toString()}`,
+        pass: false,
+      }
+    }
+  },
+})
+
+declare global {
+  namespace jest { // eslint-disable-line @typescript-eslint/no-namespace
+    interface Matchers<R> {
+      toExactlyMatchN(b: N): R
+    }
+  }
+}
+
+describe('N: BigInt tests', () => {
+
+  const A = () => new N(10000n, 2)    // 100.00
+  const B = () => new N(2345000n, 5)  //  23.45000
+  const C = () => new N(123450n, 3)   // 123.450
+
+  const D = () => new N(500n, 1)      // 50.0
+  const E = () => new N(200000n, 5)   //  2.00000
+
+  /* Construction */
+
+  describe('Construction', () => {
+
+    const control = A()
+
+    test(`new N(number)`, () => {
+      const result = new N(10000, 2)
+      expect(result).toExactlyMatchN(control)
+    })
+
+    test(`new N(bigint)`, () => {
+      const result = new N(10000n, 2)
+      expect(result).toExactlyMatchN(control)
+    })
+
+    test(`new N(string)`, () => {
+      const result = new N('10000', 2)
+      expect(result).toExactlyMatchN(control)
+    })
+
+    test(`new N(decimal string)`, () => {
+      const result = new N('100.00')
+      expect(result).toExactlyMatchN(control)
+    })
+
+    test(`new N(N)`, () => {
+      const a = new N(100n)
+
+      const result = new N(a)
+      expect(result).toExactlyMatchN(control)
+    })
+
+    test(`new N({ isPrecise: true })`, () => {
+      const a = new N(123456789000000000n, 2, 10, { isPrecise: true })
+
+      const result = a.toString()
+      const expected = '12345678.90'
+
+      expect(result).toEqual(expected)
+    })
+
+    test(`N.clone()`, () => {
+      const a = new N(100n)
+
+      const result = new N(a)
+      expect(result).toExactlyMatchN(control)
+    })
+
+  })
+
+  /* Methods */
+
+  describe('Methods', () => {
+
+    test(`N.valueOf()`, () => {
+      const a = A()
+
+      const result = a.valueOf()
+      const expected = 100n
+
+      expect(result === expected).toBeTruthy()
+    })
+
+    test(`N.toDecimal()`, () => {
+      const a = A()
+
+      const result = a.toDecimal()
+      const expected = 10000n
+
+      expect(result === expected).toBeTruthy()
+    })
+
+    test(`N.toPrecise()`, () => {
+      const a = A()
+
+      const result = a.toPrecise()
+      const expected = 100n * 10n ** BigInt(N.DEFAULT_PRECISION)
+
+      expect(result === expected).toBeTruthy()
+    })
+
+    test(`N.toString()`, () => {
+      const a = A()
+
+      const result = a.toString()
+      const expected = '100.00'
+
+      expect(result).toBe(expected)
+    })
+  })
+
+  /* Arithmetics */
+
+  describe('Arithmetics', () => {
+
+    test(`N.plus()  : A+B=C`, () => {
+      const expected = C().toPrecise()
+
+      const a = A()
+      const b = B()
+      const result = a.plus(b).toPrecise()
+
+      expect(result).toBe(expected)
+    })
+
+    test(`N.minus() : C-B=A`, () => {
+      const expected = A().toPrecise()
+
+      const a = C()
+      const b = B()
+      const result = a.minus(b).toPrecise()
+
+      expect(result).toBe(expected)
+    })
+
+    test(`N.mul()   : D×E=A`, () => {
+      const expected = A().toPrecise()
+
+      const a = D()
+      const b = E()
+      const result = a.mul(b).toPrecise()
+
+      expect(result).toBe(expected)
+    })
+
+    test(`N.div()   : A÷E=D`, () => {
+      const expected = D().toPrecise()
+
+      const a = A()
+      const b = E()
+      const result = a.div(b).toPrecise()
+
+      expect(result).toBe(expected)
+    })
+
+  })
+
+})
--- packages/bign/src/lib/bign.ts
+++ packages/bign/src/lib/bign.ts
@@ -1,3 +1,111 @@
-export function bign(): string {
-  return 'bign';
+export class N {
+
+  /** The number of decimal places for internal calculations. */
+  static DEFAULT_PRECISION = 80
+
+  /**
+   * Given float value 123.456 and DEFAULT_PRECISION = 5,
+   * we get 123456 with 3 decimals, which is then internally stored as:
+   *
+   *
+   * decimals = 3
+   * decimalsFactor = 1000
+   *
+   * value = 12345600
+   * precision = 5
+   * factor = 100000
+   */
+
+  /** The original token's decimals. Only used during wrapping and unwrapping. */
+  public readonly decimals: number
+  /** Decimals as multipliert: 10^decimals. */
+  public readonly decimalsFactor: bigint
+
+  /** Internal value is original value rebased on higher precision. */
+  public value: bigint
+  /** Internal decimal precision. */
+  public precision: number
+  /** Decimal precision as multiplier: 10^precision. */
+  public factor: bigint
+
+  constructor(value: N)
+  constructor(value: bigint, decimals: number, precision: number, options: { isPrecise: true })
+  constructor(value?: string | number | bigint, decimals?: number, precision?: number, options?: { isPrecise?: boolean })
+  constructor(value: string | number | bigint | N = 0, decimals = 0, precision: number = N.DEFAULT_PRECISION, options?: { isPrecise?: boolean }) {
+
+    if (value instanceof N) {
+      Object.assign(this, value)
+      return
+    }
+
+    if (typeof value === 'string' && value.includes('.')) {
+      decimals = value.length - 1 - value.indexOf('.')
+      value = value.split('.').join('')
+    }
+
+    this.decimals = decimals
+    this.decimalsFactor = 10n ** BigInt(decimals)
+
+    this.precision = precision < decimals ? decimals : precision
+    this.factor = 10n ** BigInt(precision)
+
+    if (options?.isPrecise === true) {
+      this.value = BigInt(value)
+    } else {
+      this.value = BigInt(value) * this.factor / this.decimalsFactor
+    }
+  }
+
+  /** Returns the truncated BigInt value. */
+  valueOf(): bigint { return this.value / this.factor }
+
+  /** Returns the BigInt value including original decimals. */
+  toDecimal(): bigint { return this.value * this.decimalsFactor / this.factor }
+
+  /** Returns the internal value using internal precision. */
+  toPrecise(): bigint { return this.value }
+
+  /** Returns the original decimal BigInt value as string. */
+  toString(): string {
+    const precise = this.toDecimal()
+    return precise.toString().slice(0, -this.decimals) + '.' + precise.toString().slice(-this.decimals)
+  }
+
+  clone() {
+    return new N(this)
+  }
+
+  plus(Addend: N) {
+    const augend = this.value
+    const addend = this.rebase(Addend)
+    this.value = augend + addend
+    return this
+  }
+
+  minus(Subtrahend: N) {
+    const minuend = this.value
+    const subtrahend = this.rebase(Subtrahend)
+    this.value = minuend - subtrahend
+    return this
+  }
+
+  mul(Factor: N) {
+    const multiplier = this.value
+    const multiplicand = this.rebase(Factor)
+    this.value = multiplier * multiplicand / this.factor
+    return this
+  }
+
+  div(Divisor: N) {
+    const dividend = this.value
+    const divisor = this.rebase(Divisor)
+    this.value = this.factor * dividend / divisor
+    return this
+  }
+
+  /** Brings the other operand to same precision to allow mathematical interaction. */
+  private rebase(instance: N) {
+    return this.factor * instance.value / instance.factor
+  }
+
 }
