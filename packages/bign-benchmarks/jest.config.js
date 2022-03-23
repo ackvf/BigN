bfa54170297cdda92adcd5c2a1a1d41c96de8361
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qry.me>
2022-03-23 03:39:37 +0100

    init BigN-Benchmarks and Colors packages

---
+++ packages/bign-benchmarks/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'bign-benchmarks',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/bign-benchmarks',
+};
